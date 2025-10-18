d22c12a7c1100487d3e41a3f04a8585f864c575c
hojat72elect <hojat72elect@gmail.com>
2025-10-18 18:34:54 -0400

    Added vignette example.

--- examples/game2/game2.js
+++ examples/game2/game2.js
@@ -1,16 +1,46 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 class Game2 extends Phaser.Scene {
     constructor() {
-        super({ key: 'Game2' });
+        super();
     }
 
     preload() {
-        // Preload assets for Game 2
+        // Load all assets here
+        this.load.setBaseURL("https://labs.phaser.io/assets")
+        this.load.image('pic', 'pics/anime-ai.jpg');
+        this.load.image('test', 'sprites/phaser3-logo.png');
     }
 
     create() {
-        this.add.text(400, 300, 'Hello from Game 2!', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        const bg = this.add.image(400, 300, 'pic');
+        const logo = this.add.image(400, 500, 'test');
+
+        const fx = bg.preFX.addVignette(0.5, 0.5, 0, 0.5);
+        const fx2 = logo.preFX.addVignette(0.5, 0.5, 0, 0.2);
+
+        const text = this.add.text(10, 10, 'FX.Vignette.radius: 0');
+
+        this.tweens.add({
+            targets: [ fx, fx2 ],
+            radius: 1,
+            duration: 4000,
+            yoyo: true,
+            loop: -1,
+            hold: 1000,
+            onUpdate: () => {
+                text.setText(`FX.Vignette.radius: ${fx.radius}`);
+            }
+        });
+
+        this.tweens.add({
+            targets: logo,
+            y: 550,
+            duration: 1000,
+            yoyo: true,
+            loop: -1,
+            ease: 'sine.inout'
+        });
     }
 }
 
@@ -18,7 +48,8 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [Game2]
+    backgroundColor: '#000000',
+    scene: Game2
 };
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
