3ff2a0d61974359c53048cda57391b8ac732b20b
hojat72elect <hojat72elect@gmail.com>
2025-10-22 02:47:14 -0400

    Added barrel sprite effect.

---
+++ examples/effects/barrel/BarrelSpriteFilter.js
@@ -0,0 +1,37 @@
+import Phaser from "phaser";
+
+class Example extends Phaser.Scene {
+    preload() {
+        this.load.setBaseURL("https://labs.phaser.io/assets");
+        this.load.image('block', 'pics/lance-overdose-loader-eye.png');
+    }
+
+    create() {
+        let amount = 0.5;
+
+        for (let y = 0; y < 2; y++) {
+            for (let x = 0; x < 3; x++) {
+                const sprite = this.add.sprite(128 + x * 256, 128 + y * 256, 'block');
+
+                sprite.preFX.setPadding(32);
+
+                sprite.preFX.addBarrel(amount);
+
+                this.add.text(128 + x * 256, 128 + y * 256 + 96, amount).setOrigin(0.5, 0).setResolution(window.devicePixelRatio);
+
+                amount += 0.25;
+            }
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#47255b',
+    parent: 'phaser-example',
+    scene: Example
+};
+
+new Phaser.Game(config);
