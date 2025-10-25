9e1216f60a3af52fdc97a213c7884cae5339f30a
hojat72elect <hojat72elect@gmail.com>
2025-10-24 23:08:32 -0400

    Added Example - Camera - Rotate Camera.

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
                 textures_texture_packer_atlas: 'TexturePackerAtlas.html',
                 camera_cross_scene_object_placement: "CrossSceneObjectPlacement.html",
                 camera_ignore_game_objects: "IgnoreGameObjects.html",
+                camera_rotate_camera: "RotateCamera.html"
             }
         }
     },
